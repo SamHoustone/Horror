6414b36d732b10b60297a1e2127a0b69371a4b9e
bladtherf <bladtherf@gmail.com>
2021-05-23 22:46:31 +0530

    .

--- Horror/Assets/CarScene/CarSceneCrashTrigger.cs
+++ Horror/Assets/CarScene/CarSceneCrashTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CarSceneCrashTrigger : MonoBehaviour
 {
-    public GameObject rock1,rock2,rock3,rock4;
+    public GameObject rock1,rock2,rock3;
     public CameraShake cameraShake;
     public GameObject Timeline;
     private void OnTriggerEnter(Collider other) {
@@ -14,7 +14,6 @@ public class CarSceneCrashTrigger : MonoBehaviour
             rock1.SetActive(true);
             rock2.SetActive(true);
             rock3.SetActive(true);
-            rock4.SetActive(true);
             Timeline.SetActive(true);
 
         }
