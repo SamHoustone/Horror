5513969f7d7e32847760caa4a4f549f0e79f566f
Horror Game <83822239+Karthicksubburaj@users.noreply.github.com>
2021-06-13 14:56:32 +0530

    cha

---
+++ Horror/Assets/TriggerActi.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerActi : MonoBehaviour
+{
+
+    public GameObject obj1;
+    public GameObject obj2;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            obj1.SetActive(true);
+            obj2.SetActive(false);
+            Destroy(obj2);
+            Destroy(gameObject);
+        }
+    }
+}
